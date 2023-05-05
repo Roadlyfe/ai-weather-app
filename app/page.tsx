82126618dabb4c5bbefde748ea6e1fb15a182b7b
Adam Smith <adam@roadlyfe.com>
2023-05-05 15:40:04 -0700

    initialized and styled CityPicker Component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,21 @@
 'use client'
 
+import CityPicker from "@/components/CityPicker";
 import { Card, Divider, Subtitle, Text } from "@tremor/react"
 
 export default function Home() {
   return (
-    <main className="text-6xl">
-      <h1>Lets build a GPT-4 Weather app</h1>
-    </main>
+    <div  className="min-h-screen bg-gradient-to-br from-[#2ec5c5] to-[#183B7E] p-10 flex flex-col justify-center items-center">
+      <Card className="max-w-4xl mx-auto"> 
+        <Text className="text-6xl font-bold text-center mb-10">Weather AI</Text>
+      <Subtitle className="text-xl text-center">
+        Powered by OpenAI, Next.js 13.3, Tailwind CSS, Tremor 2.0 and More!
+      </Subtitle>
+      <Divider className="my-10"/>
+      <Card className="bg-gradient-to-br from-[#2ec5c5] to-[#183B7E]">
+       <CityPicker />
+      </Card>
+      </Card>
+    </div>
   );
 }
